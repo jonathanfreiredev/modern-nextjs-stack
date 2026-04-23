aecd14031a124149a9adf846515978bae9dc4aee
Jonathan Freire <jonathan.freire@wandace.com>
2026-04-23 17:00:09 +0200

    Add prisma and better-auth

---
+++ lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { db } from "./db";
+
+export const auth = betterAuth({
+  baseURL: {
+    allowedHosts: ["localhost:3000", "*.vercel.app"],
+    protocol: process.env.NODE_ENV === "development" ? "http" : "https",
+  },
+  database: prismaAdapter(db, {
+    provider: "postgresql", // or "sqlite" or "mysql"
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
+
+export type Session = typeof auth.$Infer.Session;
